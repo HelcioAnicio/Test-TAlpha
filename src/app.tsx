3204c1fada7f4173cc7cb3135141d113383b308b
Helcio <helcioanicio19@gmail.com>
2024-08-12 09:05:38 -0300

    Feat: Used react router dom

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { Header } from "./Components/Header/header";
 import { Login } from "./Components/Login/login";
 import { Register } from "./Components/Register/register";
 
 function App() {
+  const [token, setToken] = useState<string | null>(null);
+
+  const handleLoginSuccess = (token: string) => {
+    setToken(token);
+  };
+
   return (
     <div className='w-full min-h-screen bg-slate-950 text-white'>
       <Header />
       <main>
-        <Login />
+        {!token ? (
+          <Login onLoginSuccess={handleLoginSuccess} />
+        ) : (
+          <p>Bem vindo</p>
+        )}
         <Register />
       </main>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Products } from "./Pages/products.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/products",
+    element: <Products />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
